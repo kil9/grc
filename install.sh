01c626f0a97765ae526aa3b6e31043c2b32dc900
chungha <chungha.kim@nhn.com>
2013-11-06 18:59:06 +0900

    Fix an installer bug

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ APP_NAME='grc portable'
 APP_HOME="`dirname "$0"`"
 APP_HOME=`cd "$APP_HOME" > /dev/null; pwd`
 
-echo "export GRCHOME=$GRCHOME # grc" >> $HOME/.bashrc
+echo "export GRCHOME=$APP_HOME # grc" >> $HOME/.bashrc
 echo 'export PATH=$GRCHOME/bin:$PATH # grc' >> $HOME/.bashrc
 
 echo 'install complete'
